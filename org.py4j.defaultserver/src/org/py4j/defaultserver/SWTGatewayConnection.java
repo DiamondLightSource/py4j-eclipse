3bfafb8db38fa940785c0f4536fb050cf6adef70
Peter Chang <peter.chang@diamond.ac.uk>
2019-10-24 16:37:14 +0100

    Fix up port of default server to version 0.9.0

--- org.py4j.defaultserver/src/org/py4j/defaultserver/SWTGatewayConnection.java
+++ org.py4j.defaultserver/src/org/py4j/defaultserver/SWTGatewayConnection.java
@@ -57,7 +57,7 @@ public class SWTGatewayConnection extends GatewayConnection {
 			logger.log(Level.WARNING,
 					"Error occurred while waiting for a command.", e);
 			if (executing && writer != null) {
-				quietSendFatalError(writer, e);
+				quietSendError(writer, e);
 			}
 		} finally {
 			NetworkUtil.quietlyClose(socket);
